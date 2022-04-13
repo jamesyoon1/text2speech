4fa043d5744a913fa5c18c8f1c4587b4bcdb7fe0
James Yoon <65934713+jamesyoon1@users.noreply.github.com>
2022-04-13 10:32:07 -0400

    Add files via upload

---
+++ DeliNumber/textToSpeech.js
@@ -0,0 +1,42 @@
+
+   
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "en";
+
+//get voices
+let voices = [];
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
+  let voiceSelect = document.querySelector("#voices");
+  //voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+  
+  // Google US English
+  speech.voice = voices[49];
+
+};
+
+// start speech
+document.querySelector("#start").addEventListener("click", () => {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
+
+// wait function as existing javascript wait function was problematic 
+function wait(ms)
+{
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while(d2-d < ms);
+}
+
+// speak from user input
+function speak(num) {
+
+  //wait 2.5 seconds to wait for ding.mp3
+  wait(2500);
+
+  speech.text = num;
+  window.speechSynthesis.speak(speech);
+}
